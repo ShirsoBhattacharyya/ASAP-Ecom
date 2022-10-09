30c1629ed1a2285398f046821c947cd8a44eccd9
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2022-10-10 00:41:41 +0530

    single products page structure payment gateway updated

--- backend/src/components/features/products/product.route.js
+++ backend/src/components/features/products/product.route.js
@@ -16,4 +16,12 @@ app.get('/:category',async(req,res)=>{
         res.status(401).send(e.message);
     }
 })
+app.get('/:category/:id',async(req,res)=>{
+    try{
+        let prod=await Product.find({_id:req.params.id,category:req.params.category})
+        res.send(prod);
+    }catch(e){
+        res.status(401).send(e.message);
+    }
+})
 module.exports=app
